35aaa21e1abb13fa6b95167c8ce4577af74fbe1e
Alejandro Chacón <maledict@protonmail.com>
2024-11-20 17:26:09 -0600

    frontend deployment origin

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', env('FRONTEND_URL', 'http://localhost:5173')],
+    'allowed_origins' => ['http://localhost:5173', 'https://alefchacon.github.io', env('FRONTEND_URL', 'https://alefchacon.github.io')],
 
     'allowed_origins_patterns' => [],
 
